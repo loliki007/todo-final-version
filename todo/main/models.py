893924c19ae80593a56c11387373aaaef27a0751
Leila <lolikibeishembieva@gmail.com>
2021-01-23 22:47:38 +0600

    added todo model and admin

--- todo/main/models.py
+++ todo/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class ToDo(models.Model):
+    text = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+    
+
 # Create your models here.
