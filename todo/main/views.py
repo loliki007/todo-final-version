caf8af1f581a99bf060c961de0d47c3a47acc20e
Leila <lolikibeishembieva@gmail.com>
2021-01-26 01:20:34 +0600

    added todo closing

--- todo/main/views.py
+++ todo/main/views.py
@@ -29,5 +29,11 @@ def mark_todo(request, id):
     todo.save()
     return redirect(test)
 
+def close_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed = not todo.is_closed
+    todo.save()
+    return redirect(test)
+
 
 # Create your views here.
--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path("add-todo/", add_todo, name="add-todo"),
     path("delete-todo/<id>/", delete_todo, name="delete-todo"),
     path("mark-todo/<id>/", mark_todo, name="mark-todo"),
+    path("close-todo/<id>", mark_todo, name="close-todo"),
     ]
    
    
